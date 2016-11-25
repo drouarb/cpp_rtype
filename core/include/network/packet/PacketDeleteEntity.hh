5fcde3884fafee0dc42de8caff44e2f09b3ef48f
celeri_y <yohann.celerien@epitech.eu>
2016-11-25 22:12:52 +0100

    Packet delete

--- core/include/network/packet/PacketDeleteEntity.hh
+++ core/include/network/packet/PacketDeleteEntity.hh
@@ -6,10 +6,25 @@
 #define CPP_RTYPE_PACKETDELETEENTITY_HH
 
 
-class PacketDeleteEntity
-{
+#include <network/packet/APacket.hh>
 
-};
+namespace network {
+ namespace packet {
+     class PacketDeleteEntity : public APacket {
+        virtual ~PacketDeleteEntity();
+        PacketDeleteEntity(uint32_t tick = 0,uint32_t eventId = 0, uint16_t entityId);
+         void setTick(uint32_t tick);
+         uint32_t getTick() const;
+         void setEventId(uint16_t id);
+
+         virtual uint16_t getEventId() const = 0;
+         void setEntityId(uint16_t entityId);
+         uint16_t getEntityId() const;
+
+
+     };
+ }
+}
 
 
 #endif //CPP_RTYPE_PACKETDELETEENTITY_HH
