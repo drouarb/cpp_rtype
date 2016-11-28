e2d9d8bea198529b3740b46a7d38faf289b4fd75
celeri_y <yohann.celerien@epitech.eu>
2016-11-28 13:49:32 +0100

    Packet GameList LeaderBoard

--- core/include/network/packet/PacketGameList.hh
+++ core/include/network/packet/PacketGameList.hh
@@ -5,11 +5,17 @@
 #ifndef CPP_RTYPE_PACKETGAMELIST_HH
 #define CPP_RTYPE_PACKETGAMELIST_HH
 
+#include <network/packet/APacket.hh>
 
-class PacketGameList
-{
-
-};
-
+namespace network {
+    namespace packet {
+        class PacketGameList : public APacket {
+        public:
+            PacketGameList(std::vector<std::pair<uint8_t, uint16_t >> GameList = {});
+            virtual ~PacketGameList();
+            std::vector<std::pair<uint8_t, uint16_t >> getGameList() const;
+        };
+    }
+}
 
 #endif //CPP_RTYPE_PACKETGAMELIST_HH
--- core/src/network/packet/PacketLeaderBoard.cpp
+++ core/src/network/packet/PacketLeaderBoard.cpp
@@ -2,4 +2,36 @@
 // Created by celeriy on 23/11/16.
 //
 
+#include <network/utils/NetworkVector.hh>
 #include "network/packet/PacketLeaderBoard.hh"
+network::packet::PacketLeaderBoard::PacketLeaderBoard(std::vector<std::pair<uint32_t, std::string>> LeaderBoard):
+        APacket(
+                LEADER_BOARD,
+                {
+                        new utils::NetworkVector(2, {
+                                new utils::NetworkInteger(1),
+                                new utils::NetworkString
+                        })
+                }
+        )
+{
+    for (size_t i = 0; i < LeaderBoard.size(); i++) {
+        SET_NETINT(GET_NETVEC(schema, 0)[i], 0, LeaderBoard[i].first);
+        SET_NETSTR(GET_NETVEC(schema, 0)[i], 1, LeaderBoard[i].second);
+    }
+}
+
+network::packet::PacketLeaderBoard::~PacketLeaderBoard()
+{ }
+
+std::vector<std::pair<uint32_t, std::string>> network::packet::PacketLeaderBoard::getLeaderBoard() const {
+    std::vector<std::pair<uint32_t, std::string>> list;
+
+    for (size_t i = 0; i < GET_NETVEC(schema, 0).size(); i++) {
+        list.push_back({
+                               GET_NETINT(GET_NETVEC(schema, 0)[i], 0),
+                               GET_NETSTR(GET_NETVEC(schema, 0)[i], 1)
+                       });
+    }
+    return (list);
+}
