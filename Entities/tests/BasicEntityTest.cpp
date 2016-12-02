4d714dce2b298c88cb7adf2e64525634ec7ef7e5
Grégoire GUEMAS <gregoire.guemas@epitech.eu>
2016-12-02 16:46:34 +0100

    Entities/tests/CmakeLists => each test are now linked with it specific library
    Patch root CMakeLists to enable entities tests

---
+++ Entities/tests/BasicEntityTest.cpp
@@ -0,0 +1,11 @@
+//
+// Created by greg on 02/12/2016.
+//
+
+#include <BasicEntity.hh>
+
+int main()
+{
+    BasicEntity *basicEntity = new BasicEntity(42);
+    return 0;
+}
---
+++ Entities/tests/ProjectileTest.cpp
@@ -0,0 +1,11 @@
+//
+// Created by greg on 02/12/2016.
+//
+
+#include <Projectile.hh>
+
+int main()
+{
+    Projectile *projectile = new Projectile(42);
+    return 0;
+}
