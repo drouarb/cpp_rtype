253c87c62601b92e7b68e0e6d5a3f6a6923faf4f
Grégoire GUEMAS <gregoire.guemas@epitech.eu>
2016-12-05 11:04:00 +0100

    adding monster

---
+++ Entities/include/Monster.hh
@@ -0,0 +1,14 @@
+//
+// Created by greg on 02/12/2016.
+//
+
+#ifndef CPP_RTYPE_MONSTER_HH
+#define CPP_RTYPE_MONSTER_HH
+
+
+class Monster {
+
+};
+
+
+#endif //CPP_RTYPE_MONSTER_HH
---
+++ Entities/src/Monster.cpp
@@ -0,0 +1,5 @@
+//
+// Created by greg on 02/12/2016.
+//
+
+#include "../include/Monster.hh"
