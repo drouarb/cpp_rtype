70ce1357d17c63d1c79349dc67ed1d4c0f288747
Grégoire GUEMAS <gregoire.guemas@epitech.eu>
2016-12-07 11:23:16 +0100

    Level OK + Adding extension for all shared library (see getDlLoader.hpp)

--- server/include/Level.hh
+++ server/include/Level.hh
@@ -5,19 +5,32 @@
 #include <string>
 #include <vector>
 #include <list>
+#include <map>
 
-namespace server
-{
-    class Level
-    {
+namespace server {
+    class Level {
     public:
-        Level(const std::string & filepath);
+        /**
+        * Read JSON from a the given file and translate it to a level.
+        * @note Clears existing contents of property tree.  In case of error the
+        *       property tree unmodified.
+        * @throw json_parser_error In case of error deserializing the property
+        *                          tree.
+        * @param filename Name of file from which to read in the property tree.
+        */
+        Level(const std::string &filepath);
+
         ~Level();
 
-        std::list<Spawn*> * getNewSpawns();
+        /**
+         * @param tick
+         * @return
+         */
+        const std::vector<Spawn> &getNewSpawns(round_t tick) const;
 
     private:
-        std::vector<Spawn> spawns;
+        std::map<round_t, std::vector<Spawn>> spawns;
+        std::string name;
     };
 }
 
--- server/include/LibLoader/getDlLoader.hpp
+++ server/include/LibLoader/getDlLoader.hpp
@@ -11,17 +11,20 @@
 #include "Windows.h"
 #include <tchar.h>
 #include "WinDlLoader.hpp"
+#define SHARED_LIB_EXTENTION ".dll"
 #else
 #include "UnixDlLoader.hpp"
+#define SHARED_LIB_EXTENTION ".so"
 #endif
 
 template <typename T>
 IDlLoader<T> * getDlLoader(const std::string &path)
 {
+    std::string finalPath = std::string(path + SHARED_LIB_EXTENTION);
 #ifdef _WIN32
-    return (new WinDlLoader<T>(__T(path.c_str())));
+    return (new WinDlLoader<T>(__T(finalPath.c_str())));
 #else
-    return (new UnixDlLoader<T>(path));
+    return (new UnixDlLoader<T>(finalPath));
 #endif
 }
 
