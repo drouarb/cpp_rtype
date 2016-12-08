246efc791a1f8560531875a9200de8321feba0fa
Grégoire GUEMAS <gregoire.guemas@epitech.eu>
2016-12-08 14:37:46 +0100

    Player

--- server/include/Player.hh
+++ server/include/Player.hh
@@ -5,13 +5,66 @@
 #ifndef CPP_RTYPE_PLAYER_HH
 #define CPP_RTYPE_PLAYER_HH
 
-#include "IPlayer.hh"
 #include "definitions.hh"
+#include "IPlayer.hh"
 #include "IEntity.hh"
 
 namespace server {
     class Player : public IPlayer, public IEntity {
+    private:
+        server::entityId_t id;
+        server::Team  team;
+        std::string sprite;
+        server::pos_t posX;
+        server::pos_t posY;
+        server::speed_t speedX;
+        server::speed_t speedY;
+        int hp;
+        bool destroyed;
+
+    public:
+        Player();
         void shoot(attackId_t attackId) override;
+
+        void setEntityId(entityId_t id) override;
+
+        entityId_t getEntityId() override;
+
+        Team getTeam() override;
+
+        std::string &getSprite() override;
+
+        void setPosX(pos_t pos) override;
+
+        pos_t getPosX() override;
+
+        void setPosY(pos_t pos) override;
+
+        pos_t getPosY() override;
+
+        void setSpeedX(speed_t speed) override;
+
+        speed_t getSpeedX() override;
+
+        void setSpeedY(speed_t speed) override;
+
+        speed_t getSpeedY() override;
+
+        void modHp(int i) override;
+
+        int getHp() override;
+
+        void destroy() override;
+
+        void restore() override;
+
+        bool isDestroyed() override;
+
+        void collide(IEntity *entity) override;
+
+        EntityAction *nextAction() override;
+
+
     };
 }
 
