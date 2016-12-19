a089a1a93f957d4d100a8af6db46e8e38052918e
DROUARD Benjamin <drouar_b@epitech.eu>
2016-12-19 16:42:45 +0100

    Stopwatch crossplatform

---
+++ core/include/helpers/IStopwatch.hh
@@ -0,0 +1,17 @@
+//
+// Created by drouar_b on 12/19/16.
+//
+
+#ifndef CPP_RTYPE_ISTOPWATCH_HH
+#define CPP_RTYPE_ISTOPWATCH_HH
+
+namespace helpers {
+    class IStopwatch {
+    public:
+        virtual ~IStopwatch() { }
+        virtual void set() = 0;
+        virtual long elapsedMs() = 0;
+    };
+}
+
+#endif //CPP_RTYPE_ISTOPWATCH_HH
