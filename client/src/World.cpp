247e9bfda84fa13e9240ba84200e56c91a661488
Alexis Trouve <alexis.trouve@epitech.eu>
2016-12-19 11:25:46 +0100

    debug

--- client/src/World.cpp
+++ client/src/World.cpp
@@ -27,6 +27,7 @@ World::~World()
 void	World::spawnEntity(ide_t nid, pos_t pos, typeide_t idtype, UIevent_t nevent, tick nturn)
 {
     world_mut->lock();
+    std::cout << "t=<" << nturn << "> Spawn entity (" << nid << ") type : " << idtype << " : [" << pos.first << ":" << pos.second << "]" << std::endl;
   worldEvents.insert(std::pair<tick, WorldEvent>(nturn, WorldEvent(nid, pos, idtype, nturn, nevent)));
     world_mut->unlock();
 }
@@ -34,6 +35,7 @@ void	World::spawnEntity(ide_t nid, pos_t pos, typeide_t idtype, UIevent_t nevent
 void	World::moveEntity(vec_t vec, pos_t pos, tick nturn, ide_t nid, UIevent_t nevent)
 {
     world_mut->lock();
+  std::cout << "t=<" << nturn << "> move entity (" << nid << ") : [" << pos.first << ":" << pos.second << "] vec: [" << vec.first << ":" << vec.second << "]" << std::endl;
   worldEvents.insert(std::pair<tick, WorldEvent>(nturn, WorldEvent(nid, vec, pos, nturn, nevent)));
     world_mut->unlock();
 }
@@ -41,6 +43,7 @@ void	World::moveEntity(vec_t vec, pos_t pos, tick nturn, ide_t nid, UIevent_t ne
 void	World::updateEntity(int hp, tick nturn, ide_t nid, UIevent_t nevent)
 {
     world_mut->lock();
+  std::cout << "t=<" << nturn << "> update entity (" << nid << ") : " << hp << std::endl;
   worldEvents.insert(std::pair<tick, WorldEvent>(nturn, WorldEvent(nid, hp, nturn, nevent)));
     world_mut->unlock();
 }
@@ -48,6 +51,7 @@ void	World::updateEntity(int hp, tick nturn, ide_t nid, UIevent_t nevent)
 void	World::deleteEntity(ide_t nid, tick nturn, UIevent_t nevent)
 {
     world_mut->lock();
+  std::cout << "t=<" << nturn << "> delete entity (" << nid << ")" << std::endl;
   worldEvents.insert(std::pair<tick, WorldEvent>(nturn, WorldEvent(nid, nturn, nevent)));
     world_mut->unlock();
 }
@@ -65,28 +69,33 @@ void	World::applyTurn()
   pos_t					pos;
 
   itEv = worldEvents.begin();
+  std::cout << " ----- turn(" << turn << ") -----" << std::endl;
   while (itEv->first < turn && itEv != worldEvents.end())
     {
       if (itEv->second.eventtype == SPAWN && (entitys.find(itEv->second.id) == entitys.end()))
 	{
+	  std::cout << "spawn" << std::endl;
 	  ent = new Entity(itEv->second.id, itEv->second.type, itEv->second.pos, itEv->first);
 	  pos = ent->getPos();
 	  entitys.insert(std::pair<ide_t, Entity*>(itEv->second.id, ent));
 	}
       else if (itEv->second.eventtype == UPDATE && (entitys.find(itEv->second.id) != entitys.end()))
 	{
+	  std::cout << "update" << std::endl;
 	  ent = entitys.at(itEv->second.id);
 	  pos = ent->getPos();
 	  ent->updateEntity(itEv->second.hp);
 	}
       else if (itEv->second.eventtype == MOVE && (entitys.find(itEv->second.id) != entitys.end()))
 	{
+	  std::cout << "move" << std::endl;
 	  ent = entitys.at(itEv->second.id);
 	  pos = ent->getPos();
 	  ent->moveEntity(itEv->second.vec, itEv->second.pos, itEv->second.turn);
 	}
       else if (itEv->second.eventtype == DELETE && (entitys.find(itEv->second.id) != entitys.end()))
 	{
+	  std::cout << "delete" << std::endl;
 	  ent = entitys.at(itEv->second.id);
 	  pos = ent->getPos();
 	  entitys.erase(itEv->second.id);
