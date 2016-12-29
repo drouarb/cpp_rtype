d391c361225a1a4bdcc17b4ee591a945488f4aa8
jonas_e <emile.jonas@epitech.eu>
2016-12-29 21:18:53 +0100

    audioManager update (sound)

--- client/include/UI/AudioManager.hh
+++ client/include/UI/AudioManager.hh
@@ -15,13 +15,17 @@ namespace UI {
 
         int getVolume();
         void setVolume(int newVolume);
+        int addSound(std::string path);
         void playSound(int soundID);
         void playMusic(std::string path);
+        void stopMusic();
         bool isPlaying();
         int createPlaylist();
         void addMusic(int playlistID);
     private:
         sf::Music music;
+        sf::Sound sound;
+        std::vector<sf::SoundBuffer*> buffers;
         int volume;
     };
 }
