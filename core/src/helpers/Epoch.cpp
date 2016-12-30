235b359b8e2fc0c3cf49b02637df20d725f6ff3c
Grégoire Guemas <gregoire.guemas@epitech.ey>
2016-12-30 13:14:04 +0100

    Windows Epoch (to test)

--- core/src/helpers/Epoch.cpp
+++ core/src/helpers/Epoch.cpp
@@ -2,15 +2,53 @@
 // Created by greg on 30/12/16.
 //
 
-#include <bits/time.h>
-#include <sys/time.h>
 #include "helpers/Epoch.hh"
 
 using namespace helpers;
 
 #ifdef WIN32
+uint64_t Epoch::getTimeMs() {
+ // the system time
+    SYSTEMTIME systemTime;
+    GetSystemTime( &systemTime );
+
+    // the current file time
+    FILETIME fileTime;
+    SystemTimeToFileTime( &systemTime, &fileTime );
+
+    // filetime in 100 nanosecond resolution
+    ULONGLONG fileTimeNano100;
+    fileTimeNano100 = (((ULONGLONG) fileTime.dwHighDateTime) << 32) + fileTime.dwLowDateTime;
+
+    //to milliseconds and unix windows epoche offset removed
+    ULONGLONG posixTime = fileTimeNano100/10000 - 11644473600000;
+    return posixTime;
+}
+
+uint64_t Epoch::getTimeS() {
+ // the system time
+    SYSTEMTIME systemTime;
+    GetSystemTime( &systemTime );
+
+    // the current file time
+    FILETIME fileTime;
+    SystemTimeToFileTime( &systemTime, &fileTime );
+
+    // filetime in 100 nanosecond resolution
+    ULONGLONG fileTimeNano100;
+    fileTimeNano100 = (((ULONGLONG) fileTime.dwHighDateTime) << 32) + fileTime.dwLowDateTime;
+
+    //to milliseconds and unix windows epoche offset removed
+    ULONGLONG posixTime = fileTimeNano100/10000 - 11644473600000;
+    return posixTime / 1000;
+}
 
 #else
+
+#include <bits/time.h>
+#include <sys/time.h>
+
+
 uint64_t Epoch::getTimeMs() {
     struct timeval tv;
 
