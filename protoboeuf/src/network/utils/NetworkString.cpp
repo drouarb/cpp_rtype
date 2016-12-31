a270494f188bb55f5edc1060c096563502a13969
Grégoire GUEMAS <gregoire.guemas@epitech.eu>
2016-12-31 21:25:44 +0100

    Fix Network integer and network string for windows

--- protoboeuf/src/network/utils/NetworkString.cpp
+++ protoboeuf/src/network/utils/NetworkString.cpp
@@ -4,7 +4,6 @@
 
 #include <cstring>
 #include <stdexcept>
-#include <iostream>
 #include "network/utils/NetworkString.hh"
 
 network::utils::NetworkString::~NetworkString() {
@@ -27,13 +26,13 @@ void network::utils::NetworkString::serialize(t_rawdata *data) const {
 t_rawdata::iterator network::utils::NetworkString::deserialize(t_rawdata *data, t_rawdata::iterator it) {
     unsigned short *len;
 
-    if ((&data->back() - &(*it)) < 1)
+    if (data->end() - it < 2)
         throw std::out_of_range("Can't read size of NetworkString");
     len = reinterpret_cast<unsigned short *>(&(*it));
-    if ((&data->back() - &(*it)) < *len + 1)
+    if (data->end() - it < *len + 2)
         throw std::out_of_range("Can't read string of NetworkString");
     this->resize(*len);
-	std::memcpy(const_cast<char *>(this->c_str()), &(*it) + 2, *len);
+    std::memcpy(const_cast<char *>(this->c_str()), &(*it) + 2, *len);
     return it + this->getSize();
 }
 
