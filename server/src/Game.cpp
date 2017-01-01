52d20c412132e5b571b40a48d1c496690b2037ea
Grégoire Guemas <gregoire.guemas@epitech.ey>
2017-01-01 14:15:51 +0100

    Revert "Revert "Fix windows vector problem""
    This reverts commit 188c0b029e0f58c00866da045fd7f29a6ec1d81c.

--- server/src/Game.cpp
+++ server/src/Game.cpp
@@ -381,8 +381,9 @@ void Game::moveEntities()
 
 void Game::unspawn()
 {
-    for (auto it = entities.begin(); it != entities.end();)
+    for (size_t i = 0; i < entities.size(); ++i)
     {
+        auto it = &entities.at(i);
         if ((*it)->data.getPosX() + (*it)->data.getSprite().sizeX <= FIELD_BORDER_LEFT - LEFT_MARGIN || (*it)->data.getPosX() > FIELD_BORDER_RIGHT + RIGHT_MARGIN
                 || (*it)->data.getPosY() + (*it)->data.getSprite().sizeY < Y_BORDER_WIDTH || (*it)->data.getPosY() > FIELD_HEIGHT + Y_BORDER_WIDTH)
         {
@@ -395,7 +396,8 @@ void Game::unspawn()
             this->sim_destroy(*it);
             destroyedEntities.push_back(*it);
             grid.remove(*it);
-            it = vect_erase(it, entities);
+//            it = vect_erase(it, entities);
+            entities.erase(std::find(entities.begin(), entities.end(), *it));
         }
         else
             ++it;
